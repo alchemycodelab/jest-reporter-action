487ed369b13bcebd6067fe7b180265f273861f4d
Ryan Mehta <ryan.mehta@gmail.com>
2020-04-10 09:59:19 -0700

    use escape regex

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { execSync } = require('child_process');
 try {
   const flags = getInput('flags');
   info(`##[add-matcher]${__dirname}/jest.json`)
-  const output = execSync(`npm run test -- --reporters="default" --reporters="./Reporter.js" ${flags}`, { encoding: 'utf8' });
+  const output = execSync(`npm run test -- --reporters="default" --reporters="${__dirname}/Reporter.js" ${flags}`, { encoding: 'utf8' });
   info(output);
 } catch(err) {
   setFailed(err.message);
