d35da890ae07c46021a8af5c60ba4273131ab644
Ryan Mehta <ryan.mehta@gmail.com>
2020-04-10 11:40:57 -0700

    create annotations

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ try {
   const reporter = fs.readFileSync(`${__dirname}/Reporter.js`, { encoding: 'utf8' })
     .replace('TOKEN', token)
     .replace('CHECK_RUN_ID', process.env.GITHUB_RUN_ID);
-  console.log(`${process.env.GITHUB_WORKSPACE}/Reporter.js`)
+  console.log(process.env.GITHUB_RUN_ID)
   fs.writeFileSync(`${process.env.GITHUB_WORKSPACE}/Reporter.js`, reporter);
   fs.writeFileSync(`${process.env.GITHUB_WORKSPACE}/jest.config.js`, `
 module.exports = {
