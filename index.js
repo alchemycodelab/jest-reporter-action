2b4979c75454d18181ce39c60d696d9b7cbb1b17
Ryan Mehta <ryan.mehta@gmail.com>
2020-04-10 11:02:24 -0700

    add token

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const { info, setFailed } = require('@actions/core');
+const { getInput, info, setFailed } = require('@actions/core');
 
 try {
+  const token = getInput('token');
+  console.log(token)
   info(`##[add-matcher]${__dirname}/jest.json`)
 } catch(err) {
   setFailed(err.message);
