5126d0f64d68bb1e47b3b9f414d9a7fe625f0ea7
Ryan Mehta <ryan.mehta@gmail.com>
2020-04-28 16:27:55 -0700

    fix reporter

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const { getInput, info, setFailed } = require('@actions/core');
-const fs = require('fs')
+const fs = require('fs');
+const { execSync } = require('child_process');
 
 try {
+  execSync('npm i @actions/core @actions/github');
   const token = getInput('token');
   const reporter = fs.readFileSync(`${__dirname}/Reporter.js`, { encoding: 'utf8' })
     .replace('TOKEN', token);
-  console.log(process.env.GITHUB_RUN_ID)
   fs.writeFileSync(`${process.env.GITHUB_WORKSPACE}/Reporter.js`, reporter);
   fs.writeFileSync(`${process.env.GITHUB_WORKSPACE}/jest.config.js`, `
 module.exports = {
