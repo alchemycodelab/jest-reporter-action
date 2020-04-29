6139d8e3282825793029e3bad5f8e9f0ab89517a
Ryan Mehta <ryan.mehta@gmail.com>
2020-04-28 17:15:24 -0700

    fix config

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const { execSync } = require('child_process');
 try {
   execSync('npm i @actions/core @actions/github');
   const token = getInput('token');
+  const { jest } = JSON.parse(fs.readFileSync(`${process.env.GITHUB_WORKSPACE}/package.json`, { encoding: 'utf8' }))
+  const config = {
+    ...jest,
+    reporters: ['default', '<rootDir>/Reporter.js']
+  }
   const reporter = fs.readFileSync(`${__dirname}/Reporter.js`, { encoding: 'utf8' })
     .replace('TOKEN', token);
   fs.writeFileSync(`${process.env.GITHUB_WORKSPACE}/Reporter.js`, reporter);
   fs.writeFileSync(`${process.env.GITHUB_WORKSPACE}/jest.config.js`, `
-module.exports = {
-  reporters: ['default', \`<rootDir>/Reporter.js\`]
-}`);
+module.exports = ${JSON.stringify(config)}`);
 } catch(err) {
   setFailed(err.message);
 }
